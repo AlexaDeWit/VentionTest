d2b7db9f0b1c51d3758792954855982daa9a18f2
Alexandra DeWit <alexa.dewit@gmail.com>
2019-05-17 16:25:41 +0200

    Test product and apply validators

--- Api/app/models/product.rb
+++ Api/app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, :uri, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :quality_rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 end
--- Api/test/models/product_test.rb
+++ Api/test/models/product_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "valid_product" do
+    assert_difference('Product.count', +1) do
+      Product.create(name: "Pretty Flower", uri: "localhost:3000", price: 10, quality_rating: 3)
+    end
+  end
+
+  test "invalid_price" do 
+    assert_no_changes('Product.count') do
+      Product.create(name: "Pretty Flower", uri: "localhost:3000", price: -10, quality_rating: 3)
+    end
+  end
+
+  test "invalid_qualities" do
+    assert_no_changes('Product.count') do
+      Product.create(name: "Pretty Flower", uri: "localhost:3000", price: 10, quality_rating: 6)
+      Product.create(name: "Pretty Flower", uri: "localhost:3000", price: 10, quality_rating: 0)
+    end
+
+  end
+
+  test "missing_name" do
+    assert_no_changes('Product.count') do
+      Product.create(uri: "localhost:3000", price: 10, quality_rating: 3)
+    end
+
+  end
+
+  test "missing_uri" do
+    assert_no_changes('Product.count') do
+      Product.create(name: "Pretty Flower", price: 10, quality_rating: 3)
+    end
+
+  end
+
+  test "missing_price" do
+    assert_no_changes('Product.count') do
+      Product.create(name: "Pretty Flower", uri: "localhost:3000", quality_rating: 3)
+    end
+
+  end
+
+  test "missing_quality_rating" do
+    assert_no_changes('Product.count') do
+      Product.create(name: "Pretty Flower", uri: "localhost:3000", price: 10)
+    end
+  end
+
 end
