308d2a8e514f560943bc42ef73a77b30439fdbf1
Alexandra DeWit <alexa.dewit@gmail.com>
2019-05-17 16:52:37 +0200

    Correct URIs

--- Api/db/seeds.rb
+++ Api/db/seeds.rb
@@ -3,8 +3,8 @@
 # Therefore this behaviour has been simulated instead by fetching them from Vention's Github
 Product.delete_all
 Product.create([
-  {name: "Blue Flower", uri: "https://github.com/VentionCo/vention-front-end-test/blob/master/assets/blue-flower.png", price: 80, quality_rating: 4},
-  {name: "Orange Flower", uri: "https://github.com/VentionCo/vention-front-end-test/blob/master/assets/orange-flower.png", price: 17.60, quality_rating: 3},
-  {name: "Pink Flower", uri: "https://github.com/VentionCo/vention-front-end-test/blob/master/assets/pink-flower.png", price: 40, quality_rating: 5}
+  {name: "Blue Flower", uri: "https://raw.githubusercontent.com/VentionCo/vention-front-end-test/master/assets/blue-flower.png", price: 80, quality_rating: 4},
+  {name: "Orange Flower", uri: "https://raw.githubusercontent.com/VentionCo/vention-front-end-test/master/assets/orange-flower.png", price: 17.60, quality_rating: 3},
+  {name: "Pink Flower", uri: "https://raw.githubusercontent.com/VentionCo/vention-front-end-test/master/assets/pink-flower.png", price: 40, quality_rating: 5}
 ])
 
