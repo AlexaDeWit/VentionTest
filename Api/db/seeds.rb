6b49c2c3b4bafbebd3ef716e007697b56157f8b7
Alexandra DeWit <alexa.dewit@gmail.com>
2019-05-17 16:52:02 +0200

    Seed and provide data. Also re-introduce job runner

--- Api/db/seeds.rb
+++ Api/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# In principle these image assets would be hosted on a CDN such as an S3 bucket
+# Provisioning cloud resources is outside of the scope of this project
+# Therefore this behaviour has been simulated instead by fetching them from Vention's Github
+Product.delete_all
+Product.create([
+  {name: "Blue Flower", uri: "https://github.com/VentionCo/vention-front-end-test/blob/master/assets/blue-flower.png", price: 80, quality_rating: 4},
+  {name: "Orange Flower", uri: "https://github.com/VentionCo/vention-front-end-test/blob/master/assets/orange-flower.png", price: 17.60, quality_rating: 3},
+  {name: "Pink Flower", uri: "https://github.com/VentionCo/vention-front-end-test/blob/master/assets/pink-flower.png", price: 40, quality_rating: 5}
+])
+
