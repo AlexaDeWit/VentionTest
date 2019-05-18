59a192ba5fe1912a966a04dea6bd83d2f9b94921
Alexandra DeWit <alexa.dewit@gmail.com>
2019-05-18 10:29:05 +0200

    Link to readme

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -8,12 +8,22 @@ function Home() {
         <p>
           Hi! Thank you for looking at my interview test.
         </p>
-        <p>
-          Explanations of rationale and approach can be found in the README.md file.
-        </p>
+
         <Link to="/products">
           View the Cards
         </Link>
+
+        <p>
+          Explanations of rationale and approach can be found in the README.md file.
+        </p>
+
+        <Link
+          to="https://github.com/AlexaDeWit/VentionTest/blob/master/README.md"
+          target="_blank"
+        >
+          View the Readme
+        </Link>
+
       </Header>
     </AppContainer>
   );
