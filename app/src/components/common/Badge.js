c75785584a75e716f7d7fcbee25b4b47a79437ba
Alexandra DeWit <alexa.dewit@gmail.com>
2019-05-18 15:32:48 +0200

    Finish assignment?

---
+++ app/src/components/common/Badge.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Badge = styled.div`
+  border-radius: 50%;
+  width: 5.5em;
+  height: 5.5em;
+  background-color:	#ef7c00;
+  color: white;
+  text-align: center;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  position: absolute;
+  left: 3em;
+  top: 3em;
+  font-size: 0.75em;
+  line-height: 5.5em;
+`;
--- app/src/components/product/Card.js
+++ app/src/components/product/Card.js
@@ -2,13 +2,13 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import PropTypes from 'prop-types';
-import { CardImage, StrongText, Price } from '../common'
+import { CardImage, StrongText, Price, Badge } from '../common'
 import QualityRating from './QualityRating'
 import CartOverlay from '../cart/CartOverlay'
 import * as Cart from '../../store/cart/reducer'
 import { Actions as CartActions } from '../../store/cart/actions'
 
-export const IdleCard = styled.div`
+export const CardContainer = styled.div`
   margin: 1em;
   padding: 2em;
 	box-shadow: 0px 0px 5px 5px rgba(0, 0, 0, 0.25);
@@ -52,10 +52,11 @@ class Card extends React.Component {
     const isInCart = !!cartItem
     const cardImage = <CardImage src={uri} alt={name} />
     return (
-      <IdleCard
+      <CardContainer
         onMouseLeave={this.handleMouseLeave}
         onMouseEnter={this.handleMouseEnter}
       >
+        {isInCart && <Badge>In Cart</Badge>}
         {hover
           ? <CartOverlay id={id} isInCart={isInCart} handleChange={this.handleChange}>{cardImage}</CartOverlay>
           : cardImage
@@ -63,7 +64,7 @@ class Card extends React.Component {
         <StrongText>{name}</StrongText>
         <Price>{`$${price.toFixed(2)}`}</Price>
         <QualityRating rating={quality_rating} />
-      </IdleCard>
+      </CardContainer>
     )
   }
 
